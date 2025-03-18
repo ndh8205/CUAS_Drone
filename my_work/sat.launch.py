994cd79211255a434b29cb3513b47aadad44b2a6
NDH <156777664+ndh8205@users.noreply.github.com>
2025-03-18 21:12:27 +0900

    Update sat.launch.py

--- my_work/sat.launch.py
+++ my_work/sat.launch.py
@@ -10,8 +10,6 @@ from os import environ
 
 from ament_index_python.packages import get_package_share_directory
 
-import xacro
-
 
 def generate_launch_description():
     # ld = LaunchDescription()
@@ -25,27 +23,7 @@ def generate_launch_description():
            'IGN_GAZEBO_RESOURCE_PATH':
            ':'.join([environ.get('IGN_GAZEBO_RESOURCE_PATH', default=''), canadarm_demos_path])}
 
-
-    urdf_model_path = os.path.join(simulation_models_path, 'models', 'canadarm', 'urdf', 'SSRMS_Canadarm2.urdf.xacro')
     leo_model = os.path.join(canadarm_demos_path, 'worlds', 'sat.world')
-
-
-    doc = xacro.process_file(urdf_model_path, mappings={'xyz' : '1.0 0.0 1.5', 'rpy': '3.1416 0.0 0.0'})
-    robot_description = {'robot_description': doc.toxml()}
-
-
-    #run_node = Node(
-    #    package="canadarm",
-    #    executable="move_joint_server",
-    #    output='screen'
-    #)
-
-    run_move_arm = Node(
-        package="canadarm",
-        executable="move_arm",
-        output='screen'
-    )
-
 
     start_world = ExecuteProcess(
         cmd=['gz sim', leo_model, '-r'],
@@ -54,56 +32,6 @@ def generate_launch_description():
         shell=True
     )
 
-
-    robot_state_publisher = Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[robot_description])
-
-    spawn = Node(
-        package='ros_ign_gazebo', executable='create',
-        arguments=[
-            '-name', 'canadarm',
-            '-topic', robot_description,
-        ],
-        output='screen'
-    )
-
-
-    # Control
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_canadarm_joint_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'canadarm_joint_trajectory_controller'],
-        output='screen'
-    )
-
-
-
     return LaunchDescription([
         start_world,
-        robot_state_publisher,
-        spawn,
-        #run_node,
-        run_move_arm,
-
-        RegisterEventHandler(
-            OnProcessExit(
-                target_action=spawn,
-                on_exit=[load_joint_state_broadcaster],
-            )
-        ),
-        RegisterEventHandler(
-            OnProcessExit(
-                target_action=load_joint_state_broadcaster,
-                on_exit=[load_canadarm_joint_controller],
-            )
-        ),
     ])
